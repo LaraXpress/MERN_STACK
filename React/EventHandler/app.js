9216961dc47adb6f6bd239e09cf50b077b925102
Mazedur (ISRT, DU) <89848156+LaraXpress@users.noreply.github.com>
2022-05-26 21:54:39 +0600

    Event Handler in React

---
+++ React/EventHandler/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import EventHandler from './components/EventHandler/index';
+
+export default function App() {
+  return (
+    <div>
+        <EventHandler/>
+    </div>
+  )
+}
